691a251e034fd797e1386622a507b8e76ed744b2
Cole Nelson <ctnelson1997@gmail.com>
2023-11-06 17:13:09 -0600

    hw8 draft

---
+++ src/components/screens/BadgerNewsScreen.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+function BadgerNewsScreen(props) {
+    return <View>
+        <Text>When I get to Step 3, I will need to be inside a nested stack navigator!</Text>
+    </View>
+}
+
+export default BadgerNewsScreen;
