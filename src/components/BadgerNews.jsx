d5ba01d1961dbf87ed3725635e4f424e2cf5248f
Cole Nelson <ctnelson1997@gmail.com>
2023-11-06 17:24:16 -0600

    typo

--- src/components/BadgerNews.jsx
+++ src/components/BadgerNews.jsx
@@ -6,7 +6,7 @@ import BadgerTabs from './navigation/BadgerTabs';
 
 export default function BadgerNews(props) {
 
-  // Just a suggestion fot Step 4! Maybe provide this to child components via context...
+  // Just a suggestion for Step 4! Maybe provide this to child components via context...
   const [prefs, setPrefs] = useState({});
 
   return (
